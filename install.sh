1ddacd77baa3d8c34cf275e389b724420105c0d8
Ned Wilson <ned@qppe.tech>
2019-04-17 12:42:44 -0700

    Made changes to put the zipped package file in the parent directory.

--- install.sh
+++ install.sh
@@ -6,9 +6,9 @@ PACKAGE=`cat PACKAGE | grep file | awk -F': ' '{print $2}' | sed -e 's/\.py//g'`
 PKGNAME="${PACKAGE}-${VERSION}.rvpkg"
 RVDIR="${HOME}/Library/Application Support/RV"
 zip ../$PKGNAME PACKAGE romeo_source_setup.py preferences.py
-cp -vf $PKGNAME /Volumes/romeo_inhouse/romeo/SHARED/sw_installs
+cp -vf ../$PKGNAME /Volumes/romeo_inhouse/romeo/SHARED/sw_installs
 if [ -e "${RVDIR}/Packages" ]; then
-    cp -vf $PKGNAME "${RVDIR}/Packages"
+    cp -vf ../$PKGNAME "${RVDIR}/Packages"
 fi
 if [ -e "${RVDIR}/Python" ]; then
     cp -vf *.py "${RVDIR}/Python"
